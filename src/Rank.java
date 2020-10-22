bf4c32a2bd04cf6ae83fbc2eeb0e00472e77e42a
Antony Joseph <70293544+Tonyzj97@users.noreply.github.com>
2020-10-22 08:58:12 -0500

    Update Rank.java

--- src/Rank.java
+++ src/Rank.java
@@ -1,4 +1,34 @@
 
-public enum Rank {
 
+public enum Rank {
+TWO ("2"),
+THREE ("3"),
+FOUR ("4"),
+FIVE ("5"),
+SIX ("6"),
+SEVEN ("7"),
+EIGHT ("8"),
+NINE ("9"),
+TEN ("10"),
+JACK ("J"),
+QUEEN ("Q"),
+KING ("K"),
+ACE ("A");
+	
+	
+	
+	private String symbol;
+
+private Rank(String symbol) {
+	// TODO Auto-generated constructor stub
+	this.symbol = symbol;
 }
+
+public String toString() {
+	
+	return symbol;
+}
+
+	
+}
+

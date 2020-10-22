eff64523bef1b982124f480f190d657320f31c32
Antony Joseph <70293544+Tonyzj97@users.noreply.github.com>
2020-10-22 08:31:08 -0500

    Update Suit.java

--- src/Suit.java
+++ src/Suit.java
@@ -1,4 +1,21 @@
 
 public enum Suit {
+	
+	CLUBS ("C"),
+	DIAMONDS ("D"),
+	HEARTS ("H"),
+	SPADES ("S");
+	
+	private String symbol;
+
+	private Suit(String symbol) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String toString( ) {
+		
+		return null;
+	}
+	
 
 }

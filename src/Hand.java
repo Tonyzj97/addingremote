116171c08f56662ef9848bb19bb2ab1e6502f7fc
Antony Joseph <70293544+Tonyzj97@users.noreply.github.com>
2020-10-24 11:53:27 -0500

    Add files via upload

---
+++ src/Hand.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Hand {
+private ArrayList<Card> cards = new ArrayList<Card>();
+
+
+	public Hand(Card[] cards) {
+		for(Card c : cards) {
+			this.cards.add(c);
+		}
+	}
+	
+	
+	public int size() {
+		
+		return cards.size();
+	}
+	
+	public Card getCard(int index) {
+		
+		return cards.get(index);
+	}
+	
+	public void addCard(Card card) {
+		cards.add(card);
+	}
+	
+	public Card playCard (int index) {
+		
+		if(cards.size() > 0) {
+		
+		Card removed = cards.get(index);
+		cards.remove(index);
+		
+		return removed;
+		}
+		
+		else {
+			return null;
+		}
+	}
+	
+	public String toString() {
+		
+		return cards.toString();
+	}
+	
+	public void sort( ) {
+		Collections.sort(cards);
+	}
+	
+	public boolean isSorted() {
+		ArrayList<Card> copy = new ArrayList<Card>();
+		copy = (ArrayList<Card>) cards.clone();
+		Collections.sort(copy);
+		
+		boolean val = copy.equals(cards);
+		if( val == true) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	}
+	
+
+}
